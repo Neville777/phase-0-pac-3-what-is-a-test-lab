5ea79e9942cf84ef0999c76c4eb5ed88b8f31ea0
neville777 <neville.james@student.moringaschool.com>
2023-09-13 11:19:53 +0300

    test-lab assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -9,7 +9,7 @@ describe("what-is-a-test", () => {
     });
   });
 
-  describe("Height", () => {
+  describe('Height', () => {
     it("is less than 40 and greater than 0", () => {
       expect(height).toBeMoreThan(0)
       expect(height).toBeLessThan(40);
@@ -23,3 +23,5 @@ describe("what-is-a-test", () => {
     });
   });
 });
+
+
